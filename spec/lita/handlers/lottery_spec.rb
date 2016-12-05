e4f9adcfb21695e6a7ef1af2720b6d67d31ff6f5
gouf <innocent.zero@gmail.com>
2016-12-05 20:02:51 +0900

    Add spec

--- spec/lita/handlers/lottery_spec.rb
+++ spec/lita/handlers/lottery_spec.rb
@@ -1,4 +1,7 @@
 require "spec_helper"
 
 describe Lita::Handlers::Lottery, lita_handler: true do
+  it { is_expected.to route('lottery') }
+  it { is_expected.to route('chusen') }
+  it { is_expected.to route('tyusen') }
 end
