95ab2dffd2da0df4c459cd263b9ebda94ed3bdbd
gouf <innocent.zero@gmail.com>
2016-12-05 20:03:15 +0900

    Create lottery handle

--- lib/lita/handlers/lottery.rb
+++ lib/lita/handlers/lottery.rb
@@ -2,6 +2,18 @@ module Lita
   module Handlers
     class Lottery < Handler
       # insert handler code here
+      route(/lottery/, :lottery)
+      route(/chusen/, :lottery)
+      route(/tyusen/, :lottery)
+
+      def lottery(response)
+        users = ENV['CSV_USER_NAMES']&.split(',')
+        return if users.nil?
+
+        sender_name = response.user&.name
+        user = users.reject { |x| x.eql?(sender_name) }.sample
+        response.reply "抽選結果: @#{user}"
+      end
 
       Lita.register_handler(self)
     end
